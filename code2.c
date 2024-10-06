00e8bb045fa6c6b0cea117d8464b1cffd12e9178
Daniel <d488h586@ku.edu>
2024-10-06 01:01:33 -0500

    fixed some (not all)  bugs and attempted to add retry functionality

--- code2.c
+++ code2.c
@@ -1,9 +1,15 @@
 #include <stdio.h>
 
+//Current issues: organization (spacing things out)
+//reminders: { before else statements, variables must be declared in advance, keep referring to datatype stuff
+
+void restart();
+
 int main() {
     int entered_scale;
     float temp;
     int target;
+    int restart;
 
     printf("What scale would you like to enter temperature in? (1-Fahrenheit, 2-Celsius, 3-Kelvin): ");
     scanf("%d", &entered_scale);
@@ -11,7 +17,21 @@ int main() {
     scanf("%f", &temp);
     printf("What unit of measurement would you like to convert to? (1-Fahrenheit, 2-Celsius, 3-Kelvin): ");
     scanf("%d", &target);
+    
+    if ((entered_scale == 3 && temp < 0 ) || (entered_scale == target )) {
+       
+        printf("Invalid input, would you like to try again? (1-Y, 0-N): ");
+        scanf("%d", restart);
+        
+        if (restart == 1) {
+            retry();
+          } else {
+                return 0;
+            }
+        
+    }
 }
+
 
 float celsius_to_fahrenheit(float temp) {
     return (9.0/5.0)*temp + 32.0;
@@ -27,4 +47,8 @@ float celsius_to_kelvin(float temp) {
 
 float kelvin_to_celsius(float temp) {
     return temp - 273.15;
+}
+
+void retry() {
+    main();
 }
