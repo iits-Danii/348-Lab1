90796d8362b589af06bf0150d33e40f5bd242ccf
Daniel <d488h586@ku.edu>
2024-10-06 20:43:48 -0500

    Finished up program functionality and fixed (hopefully) all bugs

--- code2.c
+++ code2.c
@@ -4,6 +4,7 @@
 //reminders: { before else statements, variables must be declared in advance, keep referring to datatype stuff
 
 void retry();
+void categorize_temp(float temp);
 float celsius_to_fahrenheit(float temp);
 float fahrenheit_to_celsius(float temp);
 float celsius_to_kelvin(float temp);
@@ -17,18 +18,20 @@ int main() {
     float temp;
     int target;
     int restart;
+    float  cat_temp;
 
-    printf("What scale would you like to enter temperature in? (1-Fahrenheit, 2-Celsius, 3-Kelvin): ");
+    printf("What scale would you like to enter temperature in? (1-Fahrenheit, 2-Celsius, 3-Kelvin): \n");
     scanf("%d", &entered_scale);
-    printf("What is the temperature?: ");
+    printf("What is the temperature?: \n");
     scanf("%f", &temp);
-    printf("What unit of measurement would you like to convert to? (1-Fahrenheit, 2-Celsius, 3-Kelvin): ");
+    printf("What unit of measurement would you like to convert to? (1-Fahrenheit, 2-Celsius, 3-Kelvin): \n");
     scanf("%d", &target);
     
-    if ((entered_scale == 3 && temp < 0 ) || (entered_scale == target )) {
+    if ((entered_scale == 3 && temp < 0) || (entered_scale == target) || (entered_scale < 1 || entered_scale > 3) || (target < 1 || target > 3)) {
+
        
-        printf("Invalid input, would you like to try again? (1-Y, 0-N): ");
-        scanf("%d", restart);
+        printf("Invalid input, would you like to try again? (1-Y, 0-N) \n");
+        scanf("%d", &restart);
         
         if (restart == 1) {
             retry();
@@ -37,22 +40,32 @@ int main() {
         }
         
     } else {
+
+        if (entered_scale == 1) {
+            cat_temp = fahrenheit_to_celsius(temp);
+            categorize_temp(cat_temp);
+        } else if (entered_scale == 3) {
+            cat_temp = kelvin_to_celsius(temp);
+            categorize_temp(cat_temp);
+        }
+
         float converted_temp;
+        
         if (entered_scale == 1 && target == 2) {
             converted_temp = fahrenheit_to_celsius(temp);
         } else if (entered_scale == 1 && target == 3) {
-            converted_temp == fahrenheit_to_kelvin(temp);
+            converted_temp = fahrenheit_to_kelvin(temp);
         } else if (entered_scale == 2 && target == 1) {
-            converted_temp == celsius_to_fahrenheit(temp);
+            converted_temp ==celsius_to_fahrenheit(temp);
         } else if (entered_scale == 2 && target == 3) {
             converted_temp = celsius_to_kelvin(temp);
         } else if (entered_scale == 3 && target == 1) {
-            converted_temp == kelvin_to_fahrenheit(temp);
+            converted_temp = kelvin_to_fahrenheit(temp);
         } else if (entered_scale == 3 && target == 2) {
             converted_temp = kelvin_to_celsius(temp);
         }
 
-        printf("Converted temperature: %.2f/n", converted_temp);
+        printf("Converted temperature: %.2f\n", converted_temp);
     }
 }
 
@@ -82,4 +95,18 @@ float kelvin_to_fahrenheit(float temp) {
 
 void retry() {
     main();
+}
+
+void categorize_temp(float cat_temp) {
+    if (cat_temp <= 0.0) {
+        printf("Temperature is freezing, wear winter clothing.\n");
+    } else if (cat_temp <= 10.0) {
+        printf("Temperature is cold, wear a jacket.\n");
+    } else if (cat_temp <= 25.0) {
+        printf("Temperature is comfortable, dress medium to light.\n");
+    } else if (cat_temp <= 35.0) {
+        printf("Temperature is hot, dress light.\n");
+    } else {
+        printf("Extreme heat, dress light, stay hydrated and stick to shade.\n");
+    }
 }
