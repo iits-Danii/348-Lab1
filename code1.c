048516e2312769bf92fe894292c08c4b1c8433d5
Daniel <d488h586@ku.edu>
2024-10-06 22:59:04 -0500

    Initial commit with basic main function and start on combo finding function

--- code1.c
+++ code1.c
@@ -1,2 +1,26 @@
 #include <stdio.h>
 
+
+int main() {
+    int total_score;
+
+    while (1) {
+        printf("Enter a score (must be a value greater than 1 or program will stop): ")
+        scanf("%d", &score);
+
+        if (score <= 1) {
+            break;
+        }
+
+    }
+}
+
+void find_combos(int total_score) {
+    int tdown;
+    int fieldg;
+    int safety;
+    int td1p;
+    int td2p;
+
+    printf("Possible combinations for score %d:\n")
+}
